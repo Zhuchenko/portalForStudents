5c1ee615cdab54341372dd2b7945ba82497a34ff
AndreyRi <umulazu@gmail.com>
2019-04-25 13:25:37 +0300

    add creating and starting service with promises

--- npm_scripts/createMongodbDirectory.js
+++ npm_scripts/createMongodbDirectory.js
@@ -1,14 +1,14 @@
 const mkdirp = require("mkdirp");
 const fs = require("fs");
 
-const dir = "../mongod/data/db";
+const dir = "./mongodb/data/db";
 if (!fs.existsSync(dir)){
     // creating full directory for mongodb
-    mkdirp("../mongod/data/db", (err) => {
+    mkdirp("./mongodb/data/db", (err) => {
         if (err) {
-            console.error(err);
+            console.error("Creating mongodb/data/db error!", err);
         } else {
-            console.log("/data/db exists.")
+            console.log("mongodb/data/db exists.")
         }
     });
 }
