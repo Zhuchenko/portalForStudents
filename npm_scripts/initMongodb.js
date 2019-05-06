ad1f15febc35cbdf895bea0cbe58546d330f5f04
AndreyRi <umulazu@gmail.com>
2019-05-06 12:22:47 +0300

    promise -> async/await; new instruction in readme

--- npm_scripts/initMongodb.js
+++ npm_scripts/initMongodb.js
@@ -1,10 +1,14 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/PortalForStudents", { useNewUrlParser: true }).then(() => {
-    mongoose.connection.createCollection("students");
-    mongoose.connection.createCollection("contracts").then(() => {
-        mongoose.disconnect().then(() => {
-            console.log("Connection closed.");
-        });
-    });
-    console.log("Two collections are created!");
-});
+(async () => {
+    try {
+        await mongoose.connect("mongodb://localhost:27017/PortalForStudents", {useNewUrlParser: true});
+        await mongoose.connection.createCollection("students");
+        await mongoose.connection.createCollection("contracts");
+        await mongoose.disconnect();
+        console.log("Two collections are created!");
+        console.log("Connection closed.");
+    } catch (e) {
+        console.log("When mongodb inits:");
+        console.error(e);
+    }
+})();
