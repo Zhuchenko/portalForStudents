1b9b4aa4cb0472898a31a8810c809186be27af19
Andrei R <umulazu@gmail.com>
2019-07-11 11:27:11 +0300

    add StatusTable (#5)
    * finish with branch react-creating-status-table

--- npm_scripts/initMongodb.js
+++ npm_scripts/initMongodb.js
@@ -4,8 +4,9 @@ const mongoose = require("mongoose");
         await mongoose.connect("mongodb://localhost:27017/PortalForStudents", {useNewUrlParser: true});
         await mongoose.connection.createCollection("students");
         await mongoose.connection.createCollection("contracts");
+        await mongoose.connection.createCollection("workdays");
         await mongoose.disconnect();
-        console.log("Two collections are created!");
+        console.log("Three collections are created!");
         console.log("Connection closed.");
     } catch (e) {
         console.log("When mongodb inits:");
